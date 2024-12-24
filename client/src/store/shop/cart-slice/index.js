dcdf997787c4b58326728d59737eefc9d8925c5b
Bisma Fajar <120157054+bismafajar816@users.noreply.github.com>
2024-12-24 21:54:10 +0500

    Update index.js

--- client/src/store/shop/cart-slice/index.js
+++ client/src/store/shop/cart-slice/index.js
@@ -10,7 +10,7 @@ export const addToCart = createAsyncThunk(
   "cart/addToCart",
   async ({ userId, productId, quantity }) => {
     const response = await axios.post(
-      "${backendURL}/api/shop/cart/add",
+      "https://eatheral-wear-backend.vercel.app/api/shop/cart/add",
       {
         userId,
         productId,
@@ -26,7 +26,7 @@ export const fetchCartItems = createAsyncThunk(
   "cart/fetchCartItems",
   async (userId) => {
     const response = await axios.get(
-      `${backendURL}/api/shop/cart/get/${userId}`
+      `https://eatheral-wear-backend.vercel.app/api/shop/cart/get/${userId}`
     );
 
     return response.data;
@@ -37,7 +37,7 @@ export const deleteCartItem = createAsyncThunk(
   "cart/deleteCartItem",
   async ({ userId, productId }) => {
     const response = await axios.delete(
-      `${backendURL}/api/shop/cart/${userId}/${productId}`
+      `https://eatheral-wear-backend.vercel.app/api/shop/cart/${userId}/${productId}`
     );
 
     return response.data;
@@ -48,7 +48,7 @@ export const updateCartQuantity = createAsyncThunk(
   "cart/updateCartQuantity",
   async ({ userId, productId, quantity }) => {
     const response = await axios.put(
-      "${backendURL}/api/shop/cart/update-cart",
+      "https://eatheral-wear-backend.vercel.app/api/shop/cart/update-cart",
       {
         userId,
         productId,
