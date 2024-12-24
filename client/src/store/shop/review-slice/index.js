9e03ce75c1353a291b70815542f3974d38a36c24
Bisma Fajar <120157054+bismafajar816@users.noreply.github.com>
2024-12-24 21:56:11 +0500

    Update index.js

--- client/src/store/shop/review-slice/index.js
+++ client/src/store/shop/review-slice/index.js
@@ -10,7 +10,7 @@ export const addReview = createAsyncThunk(
   "/order/addReview",
   async (formdata) => {
     const response = await axios.post(
-      `${backendURL}/api/shop/review/add`,
+      `https://eatheral-wear-backend.vercel.app/api/shop/review/add`,
       formdata
     );
 
@@ -20,7 +20,7 @@ export const addReview = createAsyncThunk(
 
 export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
   const response = await axios.get(
-    `${backendURL}/api/shop/review/${id}`
+    `https://eatheral-wear-backend.vercel.app/api/shop/review/${id}`
   );
 
   return response.data;
